0896f160e122194bb1bb3580cac1075495bd2ded
zu-min <okka.0928@gmail.com>
2017-11-02 00:38:35 +0900

    update files view

--- app.rb
+++ app.rb
@@ -4,6 +4,18 @@ require 'sinatra/reloader'
 
 Tilt.register Tilt::RedcarpetTemplate, 'md'
 
+get '/' do
+  @filenames = dir
+  markdown :"markdown_page", :layout_engine => :erb
+end
+
 get '/*' do
-  markdown :"#{params['splat'].first}"
+  @filenames = dir
+  markdown :"#{params['splat'].first}", :layout_engine => :erb
+end
+
+def dir
+  Dir.glob("./views/**/*.md").map do |file_name|
+    file_name.sub(%r(./views/), '').sub(%r(.md), '')
+  end
 end
