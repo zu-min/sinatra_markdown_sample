366c4d4c2b05a32a3c7843c94b0d0542957182ba
zu-min <okka.0928@gmail.com>
2017-11-08 19:26:23 +0900

    change markdown parser

--- app.rb
+++ app.rb
@@ -1,21 +1,23 @@
 require 'bundler/setup'
 require 'sinatra'
 require 'sinatra/reloader'
-
-Tilt.register Tilt::RedcarpetTemplate, 'md'
+require 'github/markup'
 
 get '/' do
   @filenames = dir
-  markdown :"markdown_page", :layout_engine => :erb
+  erb :top, :layout_engine => :erb
+
 end
 
 get '/*' do
   @filenames = dir
-  markdown :"#{params['splat'].first}", :layout_engine => :erb
+  file = "./views/#{params['splat'].first}"
+  htmldata = GitHub::Markup.render(file, File.read(file))
+  erb htmldata, :layout_engine => :erb
 end
 
 def dir
   Dir.glob("./views/**/*.md").map do |file_name|
-    file_name.sub(%r(./views/), '').sub(%r(.md), '')
+    file_name.sub(%r(./views/), '')
   end
 end
